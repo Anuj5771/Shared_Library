73ac15a08c9094c9b551a21750df786c4df8ba91
Anuj yadav <anujpbh2022@gmail.com>
2025-03-12 02:23:41 +0530

    Update sonarScan.groovy

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,1 +1,15 @@
-
+def call(String projectKey) {
+    stage('SonarQube Scan') {
+        withSonarQubeEnv('sonarQube-Server') {
+            withCredentials([string(credentialsId: 'sonar', variable: 'SONARQUBE_TOKEN')]) {
+                sh """
+                    sonar-scanner \
+                    -Dsonar.projectKey=${projectKey} \
+                    -Dsonar.sources=. \
+                    -Dsonar.host.url=$SONAR_HOST_URL \
+                    -Dsonar.login=$SONARQUBE_TOKEN
+                """
+            }
+        }
+    }
+}
