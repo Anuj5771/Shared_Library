a3652dd9cb50ca3d55e8a9f0951c4aea0d7f36f1
Anuj yadav <anujpbh2022@gmail.com>
2025-03-11 22:40:20 +0530

    Update sonarAnalyze.groovy

--- vars/sonarAnalyze.groovy
+++ vars/sonarAnalyze.groovy
@@ -1,1 +1,25 @@
+def call(Map params = [:]) {
+    pipeline {
+        agent any
+        stages {
+            stage('SonarQube Analysis') {
+                steps {
+                    script {
+                        echo "Starting SonarQube Analysis for project: ${params.projectKey}"
+                        
+                        // SonarQube server name (Jenkins manage Jenkins > SonarQube servers)
+                        withSonarQubeEnv('SonarQubeServer') {
+                            sh """
+                                mvn clean verify sonar:sonar \
+                                -Dsonar.projectKey=${params.projectKey} \
+                                -Dsonar.host.url=${params.hostUrl} \
+                                -Dsonar.login=${params.loginToken}
+                            """
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
