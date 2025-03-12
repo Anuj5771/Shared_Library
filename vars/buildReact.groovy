ad6f13b90528c8b328ecdf9e9a468665e62dd47f
Anuj yadav <anujpbh2022@gmail.com>
2025-03-12 20:42:34 +0530

    Update buildReact.groovy

--- vars/buildReact.groovy
+++ vars/buildReact.groovy
@@ -1,23 +1,20 @@
 def call() {
-    def NVM_DIR = '/var/lib/jenkins/.nvm'
-
     stage('Build the Application') {
-        sh """#!/bin/bash
-            export NVM_DIR="${NVM_DIR}"
-            [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
+        sh '''
+            export NVM_DIR="$HOME/.nvm"
+            source "$NVM_DIR/nvm.sh"
             nvm use 16
-            export NODE_OPTIONS=--openssl-legacy-provider
             npm install
             npm run build
-        """
+        '''
     }
 
     stage('Start the Application') {
-        sh """#!/bin/bash
-            export NVM_DIR="${NVM_DIR}"
-            [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
+        sh '''
+            export NVM_DIR="$HOME/.nvm"
+            source "$NVM_DIR/nvm.sh"
             nvm use 16
             npm start
-        """
+        '''
     }
 }
