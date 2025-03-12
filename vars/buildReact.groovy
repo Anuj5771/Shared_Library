032bf140d397ea10d658e110fbf730eb1749143f
Anuj yadav <anujpbh2022@gmail.com>
2025-03-12 20:20:55 +0530

    Update buildReact.groovy

--- vars/buildReact.groovy
+++ vars/buildReact.groovy
@@ -1,11 +1,12 @@
 def call() {
     stage('Build the Application') {
-        // Set NODE_OPTIONS for OpenSSL legacy provider
-        sh 'export NODE_OPTIONS=--openssl-legacy-provider && make build'
+        sh '''
+            export NODE_OPTIONS=--openssl-legacy-provider
+            make build
+        '''
     }
 
     stage('Start the Application') {
-        // Run npm start
         sh 'npm start'
     }
 }
