ddc24398b798ce0bd538e2c379d1c9fcf73b5944
Anuj yadav <anujpbh2022@gmail.com>
2025-03-12 19:31:12 +0530

    Update buildReact.groovy

--- vars/buildReact.groovy
+++ vars/buildReact.groovy
@@ -1,6 +1,7 @@
 def call() {
     stage('Install Dependencies') {
         sh 'npm install'
+        sh 'npm audit fix --force'   // Yeh line add kari hai
     }
     
     stage('Build React App') {
