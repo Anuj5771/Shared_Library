be2c2696bbb27aa175d0c006d781c86d614a5f12
Anuj yadav <anujpbh2022@gmail.com>
2025-03-12 20:37:23 +0530

    Update buildReact.groovy

--- vars/buildReact.groovy
+++ vars/buildReact.groovy
@@ -1,15 +1,21 @@
 def call() {
     stage('Build the Application') {
         sh '''
-            echo "Building React app with legacy OpenSSL provider..."
-            NODE_OPTIONS=--openssl-legacy-provider CI=false make build
+            export NVM_DIR="$HOME/.nvm"
+            source "$NVM_DIR/nvm.sh"
+            nvm use 16
+            export NODE_OPTIONS=--openssl-legacy-provider
+            npm install
+            npm run build
         '''
     }
 
     stage('Start the Application') {
         sh '''
-            echo "Starting app with legacy OpenSSL provider..."
-            NODE_OPTIONS=--openssl-legacy-provider npm start
+            export NVM_DIR="$HOME/.nvm"
+            source "$NVM_DIR/nvm.sh"
+            nvm use 16
+            npm start
         '''
     }
 }
