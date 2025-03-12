04557a5eb4ba3d645f56ba7c3f4a1262c39e0c8a
Anuj yadav <anujpbh2022@gmail.com>
2025-03-12 20:21:38 +0530

    Update sonarScanner.groovy

--- vars/sonarScanner.groovy
+++ vars/sonarScanner.groovy
@@ -1,14 +1,13 @@
-
 def call(String projectKey) {
     stage('SonarQube Scan') {
         withSonarQubeEnv('sonarQube-Server') { 
             withCredentials([string(credentialsId: 'demo', variable: 'SONARQUBE_TOKEN')]) {
                 sh """
-                npx sonar-scanner \
-                -Dsonar.projectKey=${projectKey} \
-                -Dsonar.host.url=${env.SONARQUBE_URL} \
-                -Dsonar.login=${SONARQUBE_TOKEN} \
-                -Dsonar.sources=src  # Specify the source directory for SonarQube
+                    npx sonar-scanner \
+                    -Dsonar.projectKey=${projectKey} \
+                    -Dsonar.host.url=${env.SONARQUBE_URL} \
+                    -Dsonar.login=${SONARQUBE_TOKEN} \
+                    -Dsonar.sources=src
                 """
             }
         }
