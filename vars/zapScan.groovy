bd4dce8ec951232f159fcdf77e0547567e983ef9
Anuj yadav <anujpbh2022@gmail.com>
2025-03-13 20:57:24 +0530

    Update zapScan.groovy

--- vars/zapScan.groovy
+++ vars/zapScan.groovy
@@ -1,9 +1,10 @@
 def call(Map params = [:]) {
     def zapURL = params.get('zapURL', 'http://default-url.com')
     def reportPath = params.get('reportPath', '/home/ubuntu/report.html')
+    def zapPort = params.get('zapPort', '10000')  // DEFAULT PORT AB 10000 KAR DIYA
 
     sh """
-        zap.sh -cmd -quickurl ${zapURL} -quickout ${reportPath}
+        zap.sh -cmd -port ${zapPort} -quickurl ${zapURL} -quickout ${reportPath}
     """
 
     echo "ZAP Scan done! Report at ${reportPath}"
