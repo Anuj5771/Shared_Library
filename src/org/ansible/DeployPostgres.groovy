1b4f6deb28af5f61b4d06c6b8b98698669714fad
Anuj yadav <anujpbh2022@gmail.com>
2025-03-27 23:32:15 +0530

    Update DeployPostgres.groovy

--- src/org/ansible/DeployPostgres.groovy
+++ src/org/ansible/DeployPostgres.groovy
@@ -4,3 +4,12 @@ class DeployPostgres {
         pipeline.sh "ansible-playbook -i aws_ec2.yml deploy_postgres.yml"
     }
 }
+
+// package org.ansible
+
+//     def call(String inventoryPath, String playbookPath, String keyPath) {
+//          stage('RunAnsible') {
+//         sh "ansible-playbook -i ${inventoryPath} ${playbookPath} --private-key=${keyPath} -u ubuntu > ansibleExecution-Report.txt"
+//         println "Ansible playbook executed: ${playbookPath}"
+//     }
+// }
