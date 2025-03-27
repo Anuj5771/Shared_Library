b0945f74db9953e1b9bd3d6a5ef1ccdcabff6fd4
Anuj yadav <anujpbh2022@gmail.com>
2025-03-27 23:30:19 +0530

    Update DeployPostgres.groovy

--- src/org/ansible/DeployPostgres.groovy
+++ src/org/ansible/DeployPostgres.groovy
@@ -1,6 +1,6 @@
 package org.ansible
 class DeployPostgres {
     def deploy(pipeline) {
-        pipeline.sh "ansible-playbook -i inventory.ini deploy_postgres.yml"
+        pipeline.sh "ansible-playbook -i aws_ec2.yml deploy_postgres.yml"
     }
 }
